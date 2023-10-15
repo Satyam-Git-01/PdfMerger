3496f35dfe61de67a7539086f55931b4bcaa9206
Satyam Choudhary <Satyamchoudhary0007@gmail.com>
2023-10-15 11:03:39 +0530

    Changes

--- merger.js
+++ merger.js
@@ -1,9 +1,13 @@
 const PDFMerger = require("pdf-merger-js");
-var merger = new PDFMerger();
+//imports done
+
+let merger = new PDFMerger(); //crearted object of PDFMerger class
 
 const mergeMethod = async (p1, p2) => {
   await merger.add(p1);
   await merger.add(p2);
   await merger.save("public/merged.pdf");
 };
+
+//exporting mergeMethoda as a module to use in server.js
 module.exports = { mergeMethod };
--- server.js
+++ server.js
@@ -4,26 +4,23 @@ const app = express();
 const multer = require("multer");
 const { mergeMethod } = require("./merger");
 const upload = multer({ dest: "uploads/" });
-const port = 5400;
-app.use("/static", express.static("public"));
+//all the neccessary imports done.
+const port = 5400; //changing port to 5400
+app.use("/static", express.static("public")); //static is being used to access public folder
+
+//sending index.html as respsonse on app start or / route
 app.get("/", (req, res) => {
-  //res.sendFile(path.join(__dirname, "index.html"))
-  res.send({
-    value: [
-      {
-        name: "Satyam",
-        rank: 100,
-      },
-      {
-        name: "Shiv",
-        rank: 200,
-      },
-    ],
-  });
-  console.log("Hello");
+  res.sendFile(path.join(__dirname, "index.html"))
 });
 
+//form submit will send data to this post method 
+//post method can accepts multiple callbacks 
 app.post("/merge", upload.array("pdfs", 2), async (req, res) => {
+
+if(req.files.length!=2){
+  res.redirect('/')
+  return ;
+}
   await mergeMethod(
     path.join(__dirname, req.files[0].path),
     path.join(__dirname, req.files[1].path)
